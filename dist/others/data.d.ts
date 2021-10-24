31dc44980bec3e92d16e7aeb55045f8b57cac17c
anarayanan345@gmail.com <anarayanan345@gmail.com>
2021-10-24 16:31:31 +0530

    remove dist, change gitignore

--- dist/others/data.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class ModifiedHolder {
-    __pattern: any;
-    private __modifiedKeys;
-    getModifiedKey: (data: string) => accountData | undefined;
-    pushModifiedKeys: (toSetData: accountData) => void;
-    getPattern(type: string): any;
-    getModifiedKeys: () => accountData[];
-}
-export declare const rawKeysList: Array<Key>;
--- dist/others/helper.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const withOutSpacesUpper: (key: Key) => accountData;
