465446d5680c281625c75c27a1f43b244f3d0874
kavin-monarch <kavinprasanth@zohomail.in>
2021-12-05 07:55:35 +0530

    [Files Removd]

--- source/others/helper.ts
+++
@@ -1,7 +0,0 @@
-export const withOutSpacesUpper = (key:Key) : accountData => {
-    return {
-        user:key.userName,
-        token:key.token.replace(/\s/g, "").toUpperCase(),
-        associateAccount:key.account,
-    }
-}
